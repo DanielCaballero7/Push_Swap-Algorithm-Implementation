14b61758bf6fb2cf60fdb11613535e2acb455fb9
dcaballe <dcaballe@student.42malaga.com>
2023-03-19 17:37:21 +0100

    libft propia

---
+++ libft/ft_atol.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaballe <dcaballe@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 12:48:36 by dcaballe          #+#    #+#             */
+/*   Updated: 2023/02/16 13:31:35 by dcaballe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	int	ft_isspace(int c)
+{
+	if (c == '\t' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r' || c == ' ')
+		return (1);
+	return (0);
+}
+
+long	ft_atol(const char *str)
+{
+	long	result;
+	int		sign;
+
+	result = 0;
+	sign = 1;
+	while (ft_isspace(*str) == 1)
+	str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (ft_isdigit(*str) == 1)
+	{
+	result = result * 10 + (*str - '0');
+	str++;
+	}
+	result *= sign;
+	return (result);
+}
---
+++ libft/ft_strjoin_free.c
@@ -0,0 +1,43 @@
+#include "libft.h"
+
+char	*make_str(char *s1, char *s2, size_t len)
+{
+	char	*s;
+	int		i;
+
+	i = 0;
+	s = (char *) malloc(sizeof(char) * len);
+	if (s == 0)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		*s++ = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		*s++ = s2[i];
+		i++;
+	}
+	*s = '\0';
+	return (s + 1 - len);
+}
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	size_t	len;
+	char	*s;
+
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	s = make_str(s1, s2, len);
+	if (s1)
+		free(s1);
+	if (s2)
+		free(s2);
+	s1 = NULL;
+	s2 = NULL;
+	return (s);
+}
